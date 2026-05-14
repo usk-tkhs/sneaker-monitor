03c70b14af6c2186c63eb9ede52309d9a60ee6ee
usk-tkhs <yusuke@remote.it>
2026-05-14 23:26:36 +0900

    fix

--- monitor_heavy.py
+++ monitor_heavy.py
@@ -207,7 +207,6 @@ def check_nike(product_key):
     api_url = (
         "https://api.nike.com/product_feed/threads/v2"
         f"?filter=productInfo.merchProduct.styleColor({style_color})"
-        "&filter=marketplace(AU)"
         "&filter=language(en-GB)"
         "&filter=channelId(d9a5bc42-4b9c-4976-858a-f159cf99c647)"
     )
